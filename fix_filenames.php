4cf79e95a8a1487b6469a29a49e4dfe199cea0a6
Filip Oščádal <f@mxd.cz>
2020-12-21 01:03:43 +0100

    minor update

--- fix_filenames.php
+++ fix_filenames.php
@@ -1,6 +1,7 @@
 <?php
 
-$ds = DIRECTORY_SEPARATOR;
+define("DS", DIRECTORY_SEPARATOR);
+
 $map = [
     'á' => 'a', 'é' => 'e', 'í' => 'i', 'ó' => 'o', 'ú' => 'u', 'ý' => 'y', 'ć' => 'c', 'č' => 'c',
     'ď' => 'd', 'ě' => 'e', 'ĺ' => 'l', 'ň' => 'n', 'ŕ' => 'r', 'ř' => 'r', 'š' => 's', 'ť' => 't',
@@ -12,56 +13,59 @@ $map = [
     "@" => '', "&" => '',
 ];
 
-$i = 0;
-$dirs = [];
-$paths = [];
-$names1 = [];
-$names2 = [];
+$work = true;
+do {
+    $i = 0;
+    $dirs = [];
+    $paths = [];
+    $names1 = [];
+    $names2 = [];
 
-echo "reading folders:\n";
-foreach ($iterator = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator("./",
-        RecursiveDirectoryIterator::SKIP_DOTS
-    ), RecursiveIteratorIterator::SELF_FIRST) as $item) {
-    $path = $iterator->getSubPath();
-    $sub = $iterator->getSubPathName();
-    $name = $iterator->getFileName();
-    $fixname = strtolower(strtr($name, $map));
-    $fixname = preg_replace('!_+!', '_', $fixname);
-    $fixname = str_replace("-_", '-', $fixname);
-    $fixname = str_replace(".-", '.', $fixname);
-    $fixname = str_replace("._", '.', $fixname);
-    $fixname = str_replace("_-", '-', $fixname);
-    $fixname = str_replace("_.", '.', $fixname);
-    $fixname = str_replace("_-_", '-', $fixname);
-    $fixname = preg_replace('!_+!', '_', $fixname);
-    if ($item->isDir()) {
-        if ($name != $fixname) {
-            $dirs[$sub] = substr_count($sub, "/");
-            $names1[$sub] = $name;
-            $names2[$sub] = $fixname;
-            $paths[$sub] = $path;
-            echo "+";
-            $i++;
-        } else {
-            echo ".";
+    clearstatcache();
+    echo "reading folders:\n";
+    foreach ($iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator("./",
+            RecursiveDirectoryIterator::SKIP_DOTS
+        ), RecursiveIteratorIterator::SELF_FIRST) as $item) {
+        $path = $iterator->getSubPath();
+        $sub = $iterator->getSubPathName();
+        $name = $iterator->getFileName();
+        $fixname = strtolower(strtr($name, $map));
+        $fixname = preg_replace('!_+!', '_', $fixname);
+        $fixname = str_replace("-_", '-', $fixname);
+        $fixname = str_replace(".-", '.', $fixname);
+        $fixname = str_replace("._", '.', $fixname);
+        $fixname = str_replace("_-", '-', $fixname);
+        $fixname = str_replace("_.", '.', $fixname);
+        $fixname = str_replace("_-_", '-', $fixname);
+        $fixname = preg_replace('!_+!', '_', $fixname);
+        if ($item->isDir()) {
+            if ($name != $fixname) {
+                $dirs[$sub] = substr_count($sub, "/");
+                $names1[$sub] = $name;
+                $names2[$sub] = $fixname;
+                $paths[$sub] = $path;
+                echo "+";
+                $i++;
+            } else {
+                echo ".";
+            }
         }
     }
-}
-
-arsort($dirs);
-if (count($dirs)) {
-    echo "\n\nfixing folders:\n";
-    foreach ($dirs??=[] as $k => $v) {
-        if ($paths[$k] == "") {
-            $paths[$k] = ".";
+    arsort($dirs);
+    if (count($dirs)) {
+        echo "\n\nfixing folders:\n";
+        foreach ($dirs??=[] as $k => $v) {
+            if ($paths[$k] == "") {
+                $paths[$k] = ".";
+            }
+            echo "> $paths[$k]/$names2[$k]\n";
+            @rename("$paths[$k]/$names1[$k]", "$paths[$k]/$names2[$k]");
         }
-        echo "> $paths[$k]/$names2[$k]\n";
-        @rename("$paths[$k]/$names1[$k]", "$paths[$k]/$names2[$k]");
+    } else {
+        $work = false;
     }
-    echo "\n\nFolders fixed. Please re-run the script.\n\n";
-    exit;
-}
+} while ($work);
 
 echo "\n\nfixing files:\n";
 foreach ($iterator = new RecursiveIteratorIterator(
@@ -90,8 +94,9 @@ foreach ($iterator = new RecursiveIteratorIterator(
         }
         if (strlen($path) && $name != $fixname) {
             echo "  * $fixname\n";
-            @rename($path . $ds . $name, $path . $ds . $fixname);
+            @rename($path . DS . $name, $path . DS . $fixname);
         }
     }
 }
+
 echo "\nDone.\n\n";
